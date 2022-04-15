1ee170c8bf1aa6c100c8f8023bd84ccb417e60cd
kesavangithub2 <kesavanbakkiya@gmail.com>
2022-04-15 15:24:25 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,25 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+
+
+
+  $("#slideshow > div:gt(0)").hide();
+
+  setInterval(function() {
+    $('#slideshow > div:first')
+      .fadeOut(1000)
+      .next()
+      .fadeIn(1000)
+      .end()
+      .appendTo('#slideshow');
+  }, 3000);
+
+
+
