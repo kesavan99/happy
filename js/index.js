a03d0ee31cab1f7947e782f9c98bcd10e07ad9bc
kesavangithub2 <kesavanbakkiya@gmail.com>
2022-04-16 12:48:02 +0530

    first commit

--- js/index.js
+++ js/index.js
@@ -8,18 +8,22 @@ function myFunction() {
   }
 
 
+  let slideIndex = 0;
+showSlides();
 
-
-  $("#slideshow > div:gt(0)").hide();
-
-  setInterval(function() {
-    $('#slideshow > div:first')
-      .fadeOut(1000)
-      .next()
-      .fadeIn(1000)
-      .end()
-      .appendTo('#slideshow');
-  }, 3000);
-
-
-
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
