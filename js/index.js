475d5d379695eb5180dcdf007bae4977480a4bea
Kesavan B <81011748+kesavan99@users.noreply.github.com>
2022-04-16 19:31:19 +0530

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,16 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+
+  
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() { $('#slideshow > div:first') .fadeOut(10) .next() .fadeIn(1000) .end() .appendTo('#slideshow'); }, 3000);
+$("#lideshow > div:gt(0)").hide();
+setInterval(function() { $('#lideshow > div:first') .fadeOut(10) .next() .fadeIn(100) .end() .appendTo('#lideshow'); }, 4000);
