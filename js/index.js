7c4f6d9f9a0fbe5f80788f4c3f22159ea52e5a71
Kesavan B <81011748+kesavan99@users.noreply.github.com>
2022-04-16 19:26:18 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -11,3 +11,5 @@ function myFunction() {
   
 $("#slideshow > div:gt(0)").hide();
 setInterval(function() { $('#slideshow > div:first') .fadeOut(10) .next() .fadeIn(1000) .end() .appendTo('#slideshow'); }, 3000);
+$("#lideshow > div:gt(0)").hide();
+setInterval(function() { $('#lideshow > div:first') .fadeOut(10) .next() .fadeIn(100) .end() .appendTo('#lideshow'); }, 4000);
